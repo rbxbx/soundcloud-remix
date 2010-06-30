d4c631222b382670c9e8b350dd44835d16236219
Lee Martin <leepaulmartin@gmail.com>
2010-06-30 09:42:03 -0700

    added edit_remix_link

--- app/helpers/remixes_helper.rb
+++ app/helpers/remixes_helper.rb
@@ -1,2 +1,13 @@
 module RemixesHelper
+  
+  def edit_remix_link
+    
+    if logged_in? && (@remix.user_id == current_user.id)
+
+      link_to("Edit", "#{@track["permalink_url"]}/edit", :target => "_blank")
+
+    end
+    
+  end
+  
 end
