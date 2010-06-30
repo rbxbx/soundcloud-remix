b0afc84e0ffb2a5f5f58c81672106f94a64915c4
Lee Martin <leepaulmartin@gmail.com>
2010-06-30 09:09:54 -0700

    added error

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,8 +4,6 @@ class VotesController < ApplicationController
     
     @remix = Remix.find(params[:id])
     @vote = Vote.new(:remix_id => @remix.id, :ip_address => request.remote_ip)
-    
-    # add JS also...
 
     respond_to do |format|
       
@@ -13,11 +11,19 @@ class VotesController < ApplicationController
         
         flash[:notice] = 'Thanks for voting!'
         format.html { redirect_to @remix }
+        format.js{
+          
+        }
         
       else
         
         flash[:notice] = 'You have already voted on this remix.'
         format.html { redirect_to @remix }
+        format.js{
+          update_page do |page|
+            page.alert = flash[:notice]
+          end
+        }
         
       end
       
