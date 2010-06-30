0a08e52ed32976687f573757debac2ff87db8800
Lee Martin <leepaulmartin@gmail.com>
2010-06-29 17:28:21 -0700

    added scope

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ActiveRecord::Base
   
   belongs_to :remix, :counter_cache => true
   
-  validates_uniqueness_of :ip_address
+  validates_uniqueness_of :ip_address, :scope => :remix_id
   
 end
