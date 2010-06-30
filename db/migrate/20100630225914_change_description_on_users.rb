7e4001ca15718be267a63572269ea0b54e8f3a17
Lee Martin <leepaulmartin@gmail.com>
2010-06-30 16:04:55 -0700

    added migration

---
+++ db/migrate/20100630225914_change_description_on_users.rb
@@ -0,0 +1,11 @@
+class ChangeDescriptionOnUsers < ActiveRecord::Migration
+
+  def self.up
+    change_column :users, :description, :text
+  end
+
+  def self.down
+    change_column :users, :description, :string
+  end
+  
+end
