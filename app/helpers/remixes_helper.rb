f49a6d037ae16122a83d61137c23fb8b9cf2cdc0
Lee Martin <leepaulmartin@gmail.com>
2010-06-30 14:11:04 -0700

    variable error fix

--- app/helpers/remixes_helper.rb
+++ app/helpers/remixes_helper.rb
@@ -2,7 +2,7 @@ module RemixesHelper
   
   def edit_remix_link
     
-    if logged_in? && (@remix.user_id == current_user.id)
+    if logged_in? && (remix.user_id == current_user.id)
 
       link_to("Edit", "#{@track["permalink_url"]}/edit", :target => "_blank")
 
