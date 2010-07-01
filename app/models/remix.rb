76b7db3152f8a6b001ebf3438da69e7815397a21
Lee Martin <leepaulmartin@gmail.com>
2010-07-01 08:23:02 -0700

    removed all references to sc track

--- app/models/remix.rb
+++ app/models/remix.rb
@@ -12,10 +12,6 @@ class Remix < ActiveRecord::Base
     HTTParty.get("http://api.soundcloud.com/tracks/#{track_id}").code == 404 ? false : true
   end
   
-  def track
-    HTTParty.get("http://api.soundcloud.com/tracks/#{track_id}")["track"]
-  end
-  
   def voted?(ip_address)
     Vote.find(:first, :conditions => {:remix_id => id, :ip_address => ip_address}) ? true : false
   end
