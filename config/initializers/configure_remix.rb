dbc6a56a946204448a510ee68d1848967997ca90
Lee Martin <leepaulmartin@gmail.com>
2010-07-05 17:17:01 -0700

    added instructions.txt and initialized

--- config/initializers/configure_remix.rb
+++ config/initializers/configure_remix.rb
@@ -3,3 +3,5 @@ require 'yaml'
 SETTINGS = YAML.load_file(File.join(Rails.root, "config", "settings.yml"))
 
 SETTINGS["group_id"] = HTTParty.get("http://api.soundcloud.com/resolve?url=#{SETTINGS["group_url"]}", :format => :xml)["group"]["id"]
+
+SETTINGS["instructions"] = File.read("#{RAILS_ROOT}/config/instructions.txt")
