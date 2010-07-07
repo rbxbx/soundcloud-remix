a6883339e257974c80be0f263c22fec68c1306db
Lee Martin <leepaulmartin@gmail.com>
2010-07-07 08:14:57 -0700

    added middleware

---
+++ app/middleware/flash_session_cookie_middleware.rb
@@ -0,0 +1,26 @@
+require 'rack/utils'
+
+class FlashSessionCookieMiddleware
+  
+  def initialize(app, session_key = '_session_id')
+    @app = app
+    @session_key = session_key
+  end
+  
+  def call(env)
+    if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+      req = Rack::Request.new(env)
+
+      unless req.params[@session_key].nil?
+        env['HTTP_COOKIE'] = "#{@session_key}=#{req.params[@session_key]}".freeze
+      end
+      
+      unless req.params['_http_accept'].nil?
+        env['HTTP_ACCEPT'] = "#{req.params['_http_accept']}".freeze
+      end
+    end
+
+    @app.call(env)
+  end
+  
+end
