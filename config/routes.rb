3300a5567ba2d2daf03c06eae5ca50733434d231
Lee Martin <leepaulmartin@gmail.com>
2010-07-08 16:12:46 -0700

    added short routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.vote 'remixes/:id/vote', :controller => 'votes', :action => 'create'
-  
-  map.status 'remixes/:id/status', :controller => 'remixes', :action => 'status'
+  map.resources :remixes
   
   map.upload 'upload', :controller => 'remixes', :action => 'new'
   
-  map.popular 'remixes/popular', :controller => 'remixes', :action => 'index', :sort => "votes_count DESC"
+  map.popular 'popular', :controller => 'remixes', :action => 'index', :sort => "votes_count DESC"
   
   map.start_upload 'start_upload', :controller => 'remixes', :action => 'start_upload'
   
-  map.resources :remixes
+  map.vote ':id/vote', :controller => 'votes', :action => 'create'
+  
+  map.status ':id/status', :controller => 'remixes', :action => 'status'
+  
+  map.remix ':id.:format', :controller => 'remixes', :action => 'show', :requirements => {:id => /\d{1,6}/}
   
   map.root :controller => "remixes"
   
