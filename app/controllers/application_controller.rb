4720da8780d14fcdc69d1772c60388a7ddab0bc5
Lee Martin <leepaulmartin@gmail.com>
2010-07-09 09:28:37 -0700

    added js login bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
   
+  def access_denied
+    respond_to do |format|
+      format.html {
+        store_location
+        redirect_to login_path
+      }
+      format.js{ render :js => "top.location.href = '/login';" }
+    end
+  end
+  
 end
