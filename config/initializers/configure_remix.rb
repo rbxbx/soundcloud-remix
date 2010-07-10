2441def67ebe8ca8eb883b27b9ace1111c009f76
Lee Martin <leepaulmartin@gmail.com>
2010-07-10 16:44:59 -0700

    added terms & conditions and renamed group_url to group

--- config/initializers/configure_remix.rb
+++ config/initializers/configure_remix.rb
@@ -2,6 +2,6 @@ require 'yaml'
 
 SETTINGS = YAML.load_file(File.join(Rails.root, "config", "settings.yml"))
 
-SETTINGS["group_id"] = HTTParty.get("http://api.soundcloud.com/resolve?url=#{SETTINGS["group_url"]}", :format => :xml)["group"]["id"]
+SETTINGS["group_id"] = HTTParty.get("http://api.soundcloud.com/resolve?url=#{SETTINGS["group"]}", :format => :xml)["group"]["id"]
 
 SETTINGS["instructions"] = File.read("#{RAILS_ROOT}/config/instructions.txt")
