3a62d8445dc81e3189ad7cfaebeaecfd09349b86
Lee Martin <leepaulmartin@gmail.com>
2010-07-12 09:51:25 -0700

    removed edit_remix link helper

--- app/helpers/remixes_helper.rb
+++ app/helpers/remixes_helper.rb
@@ -1,13 +1,2 @@
-module RemixesHelper
-  
-  def edit_remix_link(remix, track)
-    
-    if logged_in? && (remix.user_id == current_user.id)
-
-      link_to("edit", "#{track["permalink_url"]}/edit", :target => "_blank")
-
-    end
-    
-  end
-  
+module RemixesHelper  
 end
